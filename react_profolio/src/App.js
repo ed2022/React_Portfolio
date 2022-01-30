ddd5ca43891b833f283965bcbcd9bc674f9803d3
Nova <eldanadesalgne@gmail.com>
2022-01-29 19:34:30 -0500

    having the react file

---
+++ react_profolio/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
+
+const App = () => <>
+        <PortfolioContainer/> 
+        <Footer/>
+</>;
+
+export default App;
